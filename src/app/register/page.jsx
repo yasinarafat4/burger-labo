8a8e6c6814433bcbfb093901bc6fa26c635d7735
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-02-25 02:07:05 +0600

    Register page added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,10 +1,52 @@
-
+import Image from "next/image";
+import googleImage from "../../../public/google.png";
 const RegisterPage = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );
+  return (
+    <section className="mt-8 bg-white shadow-lg shadow-black/25 px-6 py-4 md:pb-16 mx-4 md:mx-36 lg:mx-60 xl:mx-80 rounded-md">
+      <h1 className="text-2xl lg:text-3xl text-center text-primary p-2 mb-8">
+        Register
+      </h1>
+      <form className="max-w-xs mx-auto">
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          //   value={email}
+          //    disabled={loginInProgress}
+          //    onChange={ev => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Your Password"
+          //   value={password}
+          //    disabled={loginInProgress}
+          //    onChange={ev => setPassword(ev.target.value)}
+        />
+        <button
+          // disabled={loginInProgress}
+          type="submit"
+        >
+          Register
+        </button>
+        <div className="divider text-gray-500">OR</div>
+        <button
+          type="button"
+          // onClick={() => signIn('google', {callbackUrl: '/'})}
+
+          className="flex gap-4 justify-center"
+        >
+          <Image
+            src={googleImage}
+            alt={"Google Login"}
+            width={24}
+            height={24}
+          />
+          Register with google
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default RegisterPage;
